4e5c6c0637b96e0386c98624e9531318cdd547e5
IEUser <IEUser@10.0.2.15>
2016-08-11 07:41:19 -0700

    Practice

---
+++ src/test/java/com/tests/AppTest.java
@@ -0,0 +1,17 @@
+package com.tests;
+
+import org.junit.Test;
+
+public class AppTest     
+{
+	
+	@Test
+	public void sampletest()
+		{
+		System.out.println("This is maven");
+		
+		}
+   
+}
+
+   
